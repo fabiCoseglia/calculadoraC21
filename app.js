81047f5ba9a9e4cb1120d8f243ceee84e73d931b
fabiCoseglia <fabiancoseglia@gmail.com>
2023-05-31 10:04:14 -0300

    inico app

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-//asdaasd
+//requires
+const argv = require('process').argv;
+
+
+//variables
+let operacion = argv[2];
+let numberA = argv[3];
+let numberB = argv[4];
+
+let resultado = 0;
+
+
+//funcionalidad
+
