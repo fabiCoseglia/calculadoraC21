eda92b191911a91dc8cd252802ba9f35dd3fcaf4
fabiCoseglia <fabiancoseglia@gmail.com>
2023-05-31 10:43:09 -0300

    sumer y restar completado

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
+const restar = require('./restar');
+const sumar = require('./sumar');
+
 //requires
 const argv = require('process').argv;
 
 
 //variables
 let operacion = argv[2];
-let numberA = argv[3];
-let numberB = argv[4];
+let numberA = +argv[3];
+let numberB = +argv[4];
 
 let resultado = 0;
 
 
 //funcionalidad
 
+if (operacion === 'sumar') {
+   console.log(sumar(numberA,numberB)); 
+}
+
+if(operacion === 'restar'){
+    console.log(restar(numberA,numberB)); 
+}
